f6fc675f62ee431733f9a8a7497bfebc96064032
mehrshad <mehrshad.nrz@gmail.com>
2025-09-23 22:08:08 +0330

    debug

--- django_core/settings.py
+++ django_core/settings.py
@@ -15,9 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
