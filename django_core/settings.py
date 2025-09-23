a32fe32c641d9bc3f44992d44bda139519c72747
mehrshad <mehrshad.nrz@gmail.com>
2025-09-23 22:04:43 +0330

    admin panel

--- django_core/settings.py
+++ django_core/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
