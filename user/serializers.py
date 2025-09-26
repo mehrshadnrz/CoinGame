70bb9ae55ad2a112d0e2f9a55df197631a084e88
mehrshad <mehrshad.nrz@gmail.com>
2025-09-26 22:07:37 +0330

    add username

--- user/serializers.py
+++ user/serializers.py
@@ -22,6 +22,7 @@ class UserSignupSerializer(serializers.ModelSerializer):
             first_name=validated_data["first_name"],
             last_name=validated_data["last_name"],
             email=validated_data["email"],
+            username=validated_data["email"],
         )
         user.set_password(validated_data["password"])
         user.save()
