3742ef678dee1241abeb9c806194a5326fd86226
mehrshad <mehrshad.nrz@gmail.com>
2025-09-29 00:46:40 +0330

    add urls

--- crypto/urls.py
+++ crypto/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import MarketStatisticsView, CryptoCoinListView,  CoinDetailView
+from .views import MarketStatisticsView, CryptoCoinListView,  CoinDetailView, CoinImportView
 
 urlpatterns = [
     path("market_statistics/", MarketStatisticsView.as_view(), name="market-statistics"),
     path("crypto_coins/", CryptoCoinListView.as_view(), name="crypto-list"),
     path("crypto_coins/<int:pk>/", CoinDetailView.as_view(), name="coin_detail"),
+    path("import-coin/", CoinImportView.as_view(), name="coin-import"),
 ]
