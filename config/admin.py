0567c55795bced26322e21a008b7a7a2fc99382e
mehrshad <mehrshad.nrz@gmail.com>
2025-10-06 21:13:50 +0330

    Admin

--- config/admin.py
+++ config/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import SiteConfig
+from .models import SiteConfig, AboutUs, ContactMessage
 
 @admin.register(SiteConfig)
 class SiteConfigAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return not SiteConfig.objects.exists()
+
+@admin.register(AboutUs)
+class AboutUsAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return not AboutUs.objects.exists()
+
+admin.site.register(ContactMessage)
